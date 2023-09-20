bbf5b545f354f162deb8a9360e9da7de371893e6
davidkakov111 <kovacsdavid648@gmail.com>
2023-09-20 18:22:51 +0300

    My_Game

--- GAME/BNB1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="BNB1" tilewidth="70" tileheight="70" tilecount="1" columns="1">
- <image source="BNB1.png" width="70" height="70"/>
-</tileset>
